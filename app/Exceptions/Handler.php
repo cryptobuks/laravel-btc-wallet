b6178e0abaab61dbfc90926394a64b9dfe7803cb
Levan Ilashvili <levan@bits.ge>
2019-09-24 14:40:56 +0400

    added global exception handler & view - html & css from 'https://codepen.io/sqfreakz/pen/GJRJOY'

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,9 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        return response()->view('errors.custom', [
+            $exception
+        ]);
+        //return parent::render($request, $exception);
     }
 }
