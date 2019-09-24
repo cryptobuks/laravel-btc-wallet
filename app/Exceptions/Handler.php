e824b40225290b17859ba3e2d1bd056b2312fccb
Levan Ilashvili <levan@bits.ge>
2019-09-24 18:11:52 +0400

    final commit

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,7 +46,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        dd($exception);
+        //dd($exception);
         return response()->view('errors.custom', [
             $exception
         ]);
